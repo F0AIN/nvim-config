ea35b2a0a6d1713f51f04fea6166fcec55e0437e
frain <F0AIN@users.noreply.github.com>
2024-08-17 15:22:21 +0900

    add orgmode

--- lua/extensions/nvim-cmp.lua
+++ lua/extensions/nvim-cmp.lua
@@ -22,6 +22,7 @@ cmp.setup {
     { name = 'luasnip' },
     { name = 'buffer' },
     { name = 'path' },
+    { name = 'orgmode' },
   },
 
   snippet = {
---
+++ lua/plugins/orgmode.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    'akinsho/org-bullets.nvim',
+    config = function()
+      require('org-bullets').setup()
+    end,
+  },
+  {
+    'nvim-orgmode/orgmode',
+    event = 'VeryLazy',
+    ft = { 'org' },
+    config = function()
+      -- Setup orgmode
+      require('orgmode').setup({
+        org_agenda_files = '~/Documents/note/**/*',
+        org_default_notes_file = '~/Documents/note/refile.org',
+      })
+    end,
+  },
+  {
+    "nvim-orgmode/telescope-orgmode.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-orgmode/orgmode",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("telescope").load_extension("orgmode")
+
+      vim.keymap.set("n", "<leader>r", require("telescope").extensions.orgmode.refile_heading)
+      vim.keymap.set("n", "<leader>oh", require("telescope").extensions.orgmode.search_headings)
+      vim.keymap.set("n", "<leader>li", require("telescope").extensions.orgmode.insert_link)
+    end,
+  },
+  {
+    "lukas-reineke/headlines.nvim",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = true, -- or `opts = {}`
+  },
+}
