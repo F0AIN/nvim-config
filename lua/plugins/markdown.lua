8633fa5dc91e18e76b3897be978aec7cd31c3ec8
frain <F0AIN@users.noreply.github.com>
2025-01-05 17:27:50 +0900

    markdownの新規作成追加

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,3 +1,15 @@
+-- 新しいMarkdownファイルを作成し、テンプレートを挿入するコマンド
+vim.api.nvim_create_user_command('NewMarkdownFile', function(opts)
+  -- 現在時刻をフォーマットしてファイル名を作成
+  local timestamp = os.date("%Y%m%d%H%M%S")
+  local filename = opts.args ~= "" and opts.args or (timestamp .. ".md")
+
+  -- 新しいバッファを開く
+  vim.cmd('edit ' .. filename)
+  -- ファイルタイプをmarkdownに設定
+  vim.bo.filetype = 'markdown'
+end, {});
+
 return {
   {
     "OXY2DEV/markview.nvim",
