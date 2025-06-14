b43898f924d22dbec8c9fdd45d3320d097550997
frain <F0AIN@users.noreply.github.com>
2025-06-14 21:53:05 +0900

    fix: markview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -16,22 +16,7 @@ return {
     lazy = false,      -- Recommended
     -- ft = "markdown" -- If you decide to lazy-load anyway
     config = function ()
-      require("markview").setup({
-        injections = {
-          languages = {
-            markdown = {
-              --- This disables other
-              --- injected queries!
-              overwrite = true,
-              query = [[
-                    (section
-                        (atx_headng) @injections.mkv.fold
-                        (#set! @fold))
-                ]]
-            }
-          }
-        }
-      });
+      require("markview").setup({});
     end,
 
     dependencies = {
