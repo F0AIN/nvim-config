ce5709f0c36ac1155957962c850f6571b58d5ecf
frain <F0AIN@users.noreply.github.com>
2025-11-23 18:04:20 +0900

    fix: Delete mason-lspconfig

--- lua/extensions/mason.lua
+++ lua/extensions/mason.lua
@@ -8,30 +8,3 @@ require("mason").setup({
     border = 'single',
   }
 })
-require('mason-lspconfig').setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = require('cmp_nvim_lsp').default_capabilities(),
-    }
-  end,
-}
-
--- An example nvim-lspconfig capabilities setting
-local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-require("lspconfig").markdown_oxide.setup({
-  -- Ensure that dynamicRegistration is enabled! This allows the LS to take into account actions like the
-  -- Create Unresolved File code action, resolving completions for unindexed code blocks, ...
-  capabilities = vim.tbl_deep_extend(
-    'force',
-    capabilities,
-    {
-      workspace = {
-        didChangeWatchedFiles = {
-          dynamicRegistration = true,
-        },
-      },
-    }
-  ),
-  on_attach = on_attach   -- configure your on attach config
-})
