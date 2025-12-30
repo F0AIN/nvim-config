882ef717a0b04753519d04491cf62ca54869f3ea
frain <F0AIN@users.noreply.github.com>
2025-12-30 22:49:29 +0900

    feat: Add obsidian.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -149,6 +149,50 @@ return {
     },
   },
 
+  -- obsidian.nvim
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "note",
+          path = "~/obsidian/Documents/Note/",
+        },
+      },
+      daily_notes = {
+        date_format = "%Y%m%d%H%M%s",
+        default_tags = { "daily" },
+        template = "_Template/Daily.md";
+      },
+      templates = {
+        folder = "_Template/",
+        date_format = "%Y%m%d%H%M%s",
+      },
+      completion = {
+        -- Set to false to disable completion.
+        nvim_cmp = true,
+        -- Trigger completion at 2 chars.
+        min_chars = 2,
+      },
+    },
+  },
+
   {
     "mvllow/modes.nvim",
     tag = "v0.2.1",
